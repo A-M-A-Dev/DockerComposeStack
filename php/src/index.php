6c5235059ecfc709cc629fa4a633b89e390ef860
Arman <armanzarei1378@gmail.com>
2020-12-09 23:09:52 -0700

    Add Redis (plus composer) to Stack

--- php/src/index.php
+++ php/src/index.php
@@ -1,6 +1,7 @@
 <?php
 include "CurlHttp.php";
 include "Router.php";
+include "vendor/predis/predis/autoload.php";
 
 define("ASP_CONTAINER", "aspnetcore");
 $url = $_SERVER['REQUEST_URI'];
@@ -9,8 +10,29 @@ $router = new Router($request);
 
 $router->get('/notes', function ($request) {
     $curl = new CurlHttp(ASP_CONTAINER);
-    return $curl->request('GET', 'notes');
+    $redis = new \Predis\Client([
+        'scheme' => 'tcp',
+        'host'   => 'redis',
+        'port'   => '6379',
+    ]);
+
+    $redisKey = 'notes-key';
+    $result = [
+        'from_cache' => true,
+        'data'       => json_decode($redis->get($redisKey)),
+    ];
+    if (!$result['data']) {
+        $data = $curl->request('GET', 'notes');
+        $result = [
+            'from_cache' => false,
+            'data'       => json_decode($data),
+        ];
+        $redis->set($redisKey, $data);
+        $redis->expire($redisKey, 30);
+    }
+    return json_encode($result);
 });
+
 $router->post('/notes', function ($request) {
     $curl = new CurlHttp(ASP_CONTAINER);
     $postData = [
